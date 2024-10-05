5ce41360d7d7617bb3784603768acb25402ca7d5
fahma20 <fahmaxo@gmail.com>
2024-10-06 00:27:21 +0300

    task-lister-code-challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      let todo = document.getElementById('new-task-description').value; 
+      buildToDo(todo);
+      form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.textContent = 'x';
+  btn.addEventListener('click', handleDelete);
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
